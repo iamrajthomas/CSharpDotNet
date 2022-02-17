a05b531bce2033ce4b2aa8876f7fc63f3e5b3727
Rajesh Thomas (External) <iamrajthomas@gmail.com>
2022-02-18 01:19:13 +0530

    Threading With Monitor and Monitor IsLockTaken

---
+++ CSharpDotNet/MultiThreadingTopic/Multithreading6_1_ThreadWithMonitor.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Threading;
+
+namespace CSharpDotNet.MultiThreadingTopic
+{
+    class Multithreading6_1_ThreadWithMonitor
+    {
+        private static object _lockObj = new object();
+        static void Main()
+        {
+            //TestCriticalPrintUsingMonitor();
+            Console.WriteLine("====================================================");
+            TestCriticalPrintUsingMonitorWithIsLockTaken();
+
+            Console.ReadLine();
+
+        }
+
+        static void TestCriticalPrintUsingMonitor()
+        {
+            Thread[] Threads = new Thread[5];
+            for (int i = 0; i < Threads.Length; i++)
+            {
+                Threads[i] = new Thread(CriticalPrintUsingMonitor);
+                Threads[i].Name = "Child Thread " + i;
+            }
+            foreach (Thread t in Threads)
+            {
+                t.Start();
+            }
+            
+        }
+
+        static void TestCriticalPrintUsingMonitorWithIsLockTaken()
+        {
+            Thread[] Threads = new Thread[5];
+            for (int i = 0; i < Threads.Length; i++)
+            {
+                Threads[i] = new Thread(CriticalPrintUsingMonitorWithIsLockTaken);
+                Threads[i].Name = "Child Thread " + i;
+            }
+            foreach (Thread t in Threads)
+            {
+                t.Start();
+            }
+
+        }
+
+        static void CriticalPrintUsingMonitor()
+        {
+            Console.WriteLine(Thread.CurrentThread.Name + " Before Trying to enter into the critical section");
+            Monitor.Enter(_lockObj);
+            try
+            {
+                Console.WriteLine(Thread.CurrentThread.Name + " Entered into the critical section");
+                for (int i = 1; i <= 15; i++)
+                {
+                    Thread.Sleep(200);
+                    Console.Write(i + ", ");
+                }
+                Console.WriteLine();
+            }
+            finally
+            {
+                Monitor.Exit(_lockObj);
+                Console.WriteLine(Thread.CurrentThread.Name + " Exit from critical section");
+            }
+        }
+
+        static void CriticalPrintUsingMonitorWithIsLockTaken()
+        {
+            Console.WriteLine(Thread.CurrentThread.Name + " Before Trying to enter into the critical section");
+            bool lockTaken = false; 
+            Monitor.Enter(_lockObj, ref lockTaken);
+            try
+            {
+                Console.WriteLine(Thread.CurrentThread.Name + " Entered into the critical section");
+                for (int i = 1; i <= 15; i++)
+                {
+                    Thread.Sleep(200);
+                    Console.Write(i + ", ");
+                }
+                Console.WriteLine();
+            }
+            finally
+            {
+                Monitor.Exit(_lockObj);
+                Console.WriteLine(Thread.CurrentThread.Name + " Exit from critical section");
+            }
+        }
+    }
+}
--- CSharpDotNet/MultiThreadingTopic/Multithreading6_ThreadLocking.cs
+++ CSharpDotNet/MultiThreadingTopic/Multithreading6_ThreadLocking.cs
@@ -7,17 +7,28 @@ namespace CSharpDotNet.MultiThreadingTopic
     {
         public void DisplayProblem()
         {
-            Console.Write("[CSharp is an ");
+            Console.Write("[A language that doesn't affect the way ");
             Thread.Sleep(5000);
-            Console.WriteLine("Object Oriented Programming Language]");
+            Console.WriteLine("you think about programming is not worth knowing. - Alan J. Perlis]");
         }
-        public void DisplaySolutionWithLock()
+        public void DisplayNonStaticSolutionWithLock()
         {
             lock (this)
             {
-                Console.Write("[CSharp is an ");
+                Console.Write("[A language that doesn't affect the way ");
                 Thread.Sleep(5000);
-                Console.WriteLine("Object Oriented Programming Language]");
+                Console.WriteLine("you think about programming is not worth knowing. - Alan J. Perlis]");
+            }
+        }
+
+        private static object _lockObj = new object();
+        public static void DisplayStaticSolutionWithLock()
+        {
+            lock (_lockObj)
+            {
+                Console.Write("[A language that doesn't affect the way ");
+                Thread.Sleep(5000);
+                Console.WriteLine("you think about programming is not worth knowing. - Alan J. Perlis]");
             }
         }
 
@@ -25,19 +36,26 @@ namespace CSharpDotNet.MultiThreadingTopic
         {
             Multithreading6_ThreadLocking obj = new Multithreading6_ThreadLocking();
 
-            //Thread T1 = new Thread(obj.DisplayProblem);
-            //Thread T2 = new Thread(obj.DisplayProblem);
-            //Thread T3 = new Thread(obj.DisplayProblem);
-            //T1.Start();
-            //T2.Start();
-            //T3.Start();
-            
-            Thread T4 = new Thread(obj.DisplaySolutionWithLock);
-            Thread T5 = new Thread(obj.DisplaySolutionWithLock);
-            Thread T6 = new Thread(obj.DisplaySolutionWithLock);
-            T4.Start();
-            T5.Start();
-            T6.Start();
+            Thread T1 = new Thread(obj.DisplayProblem);
+            Thread T2 = new Thread(obj.DisplayProblem);
+            Thread T3 = new Thread(obj.DisplayProblem);
+            T1.Start();
+            T2.Start();
+            T3.Start();
+
+            //Thread T4 = new Thread(obj.DisplayNonStaticSolutionWithLock);
+            //Thread T5 = new Thread(obj.DisplayNonStaticSolutionWithLock);
+            //Thread T6 = new Thread(obj.DisplayNonStaticSolutionWithLock);
+            //T4.Start();
+            //T5.Start();
+            //T6.Start();
+
+            //Thread T7 = new Thread(DisplayStaticSolutionWithLock);
+            //Thread T8 = new Thread(DisplayStaticSolutionWithLock);
+            //Thread T9 = new Thread(DisplayStaticSolutionWithLock);
+            //T7.Start();
+            //T8.Start();
+            //T9.Start();
 
             Console.ReadLine();
 
