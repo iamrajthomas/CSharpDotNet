8730722a761d77cd88590128b65336d3d6ca863a
Rajesh Thomas (External) <iamrajthomas@gmail.com>
2022-02-26 04:43:35 +0530

    Exception and OS Env Variables Topic

---
+++ CSharpDotNet/EnvironmentVariableTopic/TestEnvironmentVariable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDotNet.EnvironmentVariableTopic
+{
+    public class TestEnvironmentVariable
+    {
+        static void Main()
+        {
+            string Env = GetEnvVariableData();
+            Console.WriteLine("ENVIRONMENT: " + Env);
+            SetEnvVariableData();
+
+            Env = GetEnvVariableData();
+            Console.WriteLine("ENVIRONMENT: " + Env);
+
+            Console.ReadLine();
+        }
+
+        private static void SetEnvVariableData()
+        {
+            System.Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Development", EnvironmentVariableTarget.User);
+        }
+
+        private static string GetEnvVariableData()
+        {
+            return System.Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+        }
+    }
+}
---
+++ CSharpDotNet/ExceptionTopic/TestExceptionThrow.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDotNet.ExceptionTopic
+{
+    class TestExceptionThrow
+    {
+        static void Main()
+        {
+            Method1();
+
+            Console.ReadLine();
+        }
+        static void Method1()
+        {
+            try
+            {
+                Method2();
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+
+        }
+
+        private static void Method2()
+        {
+            try
+            {
+                Method3();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            
+        }
+
+        private static void Method3()
+        {
+            throw new Exception();
+        }
+    }
+    
+}
